084bddf272f9d11e49f32f91e6f1008199979919
sincerajn <sincerajn@outlook.com>
2020-03-08 20:17:33 +0800

    使用 ES 模块引入

--- node/webpack_learn/webpack.config.js
+++ node/webpack_learn/webpack.config.js
@@ -1,17 +1,15 @@
-module.exports = {
-    entry: {
-        index: "./js/index.js",
-        signup: "./js/signup.js"
-    },
-    output: {
-        filename: "[name].bundle.js",
-        path: __dirname + "/dist"
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"]
-        }]
-    },
-    mode: "development"
+export const entry = {
+    index: "./js/index.js",
+    signup: "./js/signup.js"
 }
+export const output = {
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist"
+}
+export const module = {
+    rules: [{
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+    }]
+}
+export const mode = "development"
